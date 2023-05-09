db61b1cc0b706a73076087a62e6d18dca127a797
mpaulazamin <mariapaulacoserzamin@gmail.com>
2023-05-09 15:40:47 -0300

    Results

--- treina_agente.py
+++ treina_agente.py
@@ -727,8 +727,8 @@ n_iter_checkpoints = 10
 # n_iter_checkpoints = 100
 
 # Treina e avalia o agente:
-treina = True
-avalia = False
+treina = False
+avalia = True
 
 if treina:
 
@@ -753,7 +753,7 @@ if treina:
 if avalia:
 
     # Define o concept e o selector:
-    concept = "banho_dia_quente"
+    concept = "banho_dia_ameno"
     selector = True
 
     avalia_agente(nome_algoritmo, concept, selector)
