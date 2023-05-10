d587443bad63340abc85f3420a090d73be8e08ab
Maria Paula Coser Zamin <mariapaulacoserzamin@gmail.com>
2023-05-09 22:34:58 -0300

    Update

--- treina_agente.py
+++ treina_agente.py
@@ -729,7 +729,7 @@ n_iter_checkpoints = 10
 # n_iter_checkpoints = 100
 
 # Treina e avalia o agente:
-treina = True
+treina = False
 avalia = True
 
 if treina:
@@ -766,7 +766,7 @@ if treina:
 if avalia:
 
     # Define o concept e o selector:
-    concept = "banho_noite_quente"
+    concept = "banho_dia_quente"
     selector = True
 
     avalia_agente(nome_algoritmo, concept, selector)
