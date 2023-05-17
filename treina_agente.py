5eecba7f503373ccbb7c7fd12b3894d48eee3418
mpaulazamin <mariapaulacoserzamin@gmail.com>
2023-05-16 21:47:37 -0300

    Updates

--- treina_agente.py
+++ treina_agente.py
@@ -728,8 +728,8 @@ n_iter_checkpoints = 10
 # n_iter_checkpoints = 100
 
 # Treina e avalia o agente:
-treina = True
-avalia = False
+treina = False
+avalia = True
 
 if treina:
     # Treina cada concept:
