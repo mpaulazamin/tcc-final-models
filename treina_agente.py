f8441af1eed7cb4eb9d709f38602821e4eed5b69
mpaulazamin <mariapaulacoserzamin@gmail.com>
2023-05-16 21:54:49 -0300

    Updates

--- treina_agente.py
+++ treina_agente.py
@@ -94,10 +94,10 @@ class ShowerEnv(gym.Env):
     def reset(self):
         
         # Temperatura ambiente e custo da energia elétrica em kWh:
-        self.Tinf = random.choice(self.Tinf_list)
-        self.custo_eletrico_kwh = random.choice(self.custo_eletrico_kwh_list)
-        # self.Tinf = 15
-        # self.custo_eletrico_kwh = 2.25
+        # self.Tinf = random.choice(self.Tinf_list)
+        # self.custo_eletrico_kwh = random.choice(self.custo_eletrico_kwh_list)
+        self.Tinf = 15
+        self.custo_eletrico_kwh = 2.25
 
         # Distúrbios Fd e Td, temperatura da corrente fria Tf:
         self.Fd = 0
@@ -770,7 +770,7 @@ if treina:
 if avalia:
 
     # Define o concept e o selector:
-    concept = "banho_dia_ameno"
+    concept = "banho_dia_frio"
     selector = True
 
     avalia_agente(nome_algoritmo, concept, selector)
