c6f9556c2abf42aa1b017bdefbdf6d9ba7fb7fd8
Maria Paula Coser Zamin <mariapaulacoserzamin@gmail.com>
2023-06-25 21:00:22 -0300

    More results

--- treina_agente.py
+++ treina_agente.py
@@ -757,9 +757,7 @@ if __name__ == "__main__":
         n_iter_checkpoints = 100
 
     # Define a temperatura ambiente e o custo da energia elétrica:
-    # Tinf_list = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-    # custo_eletrico_kwh_list = [1, 1.25, 1.5, 1.75, 2, 2.25]
-    Tinf_list = [30]
+    Tinf_list = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     custo_eletrico_kwh_list = [1, 1.25, 1.5, 1.75, 2, 2.25]
 
     # Treina o agente:
